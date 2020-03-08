4bb4cccbc3f6bb109c178b9fd882efe0a26098d6
ashoksri0 <Ashok.n@platform3solutions.com>
2020-03-08 20:44:12 +0530

    package added

---
+++ src/main/java/com/p3/poc/demo/ar/order/entity/Order.java
@@ -0,0 +1,35 @@
+package com.p3.poc.demo.ar.order.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.p3.poc.demo.ar.invoice.entity.Invoice;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Temporal(TemporalType.DATE)
+    private Date orderDate;
+    private String shipAddress;
+    private Double orderTotal;
+    private String orderStatus;
+    private Double orderPrice;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY,cascade= CascadeType.ALL)
+    @JoinColumn(name = "invoice_id")
+    private Invoice invoice;
+}
