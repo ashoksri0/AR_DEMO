92121cf7f2ac10eed452f3497d0345c3be120007
kilam005 <malik.sh@platform3solutions.com>
2020-03-14 08:02:43 +0530

    Extra computations

--- src/main/java/com/p3/poc/demo/ar/arsummary/ARService.java
+++ src/main/java/com/p3/poc/demo/ar/arsummary/ARService.java
@@ -9,10 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.p3.poc.demo.ar.ledger.enums.TransactionMode.AMOUNT_RECEIVABLE;
 import static com.p3.poc.demo.ar.ledger.enums.TransactionMode.AMOUNT_RECEIVED;
@@ -29,12 +27,11 @@ public class ARService {
 
     public AR360Model getAr360ViewLIst(@RequestParam final Long userID, @RequestParam(required = false) final Long invoiceID,
                                        @RequestParam(required =
-                                                  false, defaultValue = "1970-01-01") final Date startDate, @RequestParam(required = false, defaultValue = "2050-01-01") final Date endDate) {
+                                               false, defaultValue = "1970-01-01") final Date startDate, @RequestParam(required = false, defaultValue = "2050-01-01") final Date endDate) {
 
-          List<Ledger> ledgerList;
-          List<DataSetBean> amount_received_list =new ArrayList();
-          List<DataSetBean> amount_receivable_list = new ArrayList();
-
+        List<Ledger> ledgerList;
+        List<DataSetBean> amount_received_list = new ArrayList();
+        List<DataSetBean> amount_receivable_list = new ArrayList();
 
         if (invoiceID == null) {
             ledgerList =
@@ -43,25 +40,25 @@ public class ARService {
             ledgerList = ledgerRepository.findAllByUsers_IdAndInvoice_IdAndTranscationDateBetweenOrderByTranscationDateDesc(userID, invoiceID,
                     startDate, endDate);
         }
-        List<LedgerModel> ledgerModelList=new ArrayList();
+        List<LedgerModel> ledgerModelList = new ArrayList();
         ARSummary arSummary = ARSummary.builder().openingBalance(0D).totalDue(0D).totalReceivable(0D).totalReceived(0D).build();
         ledgerList.forEach(ledger -> {
-            LedgerModel ledgerModel=LedgerModel.builder().date(ledger.getTranscationDate()).invoice(ledger.getInvoice().getId()).build();
+            LedgerModel ledgerModel = LedgerModel.builder().date(ledger.getTranscationDate()).invoice(ledger.getInvoice().getId()).build();
             TransactionMode transactionMode = ledger.getTransactionMode();
             switch (transactionMode) {
                 case AMOUNT_RECEIVED:
-                   amount_received_list.add(0,DataSetBean.builder().x(ledger.getTranscationDate()).y(ledger.getTranscation()).build());
+                    amount_received_list.add(0, DataSetBean.builder().x(ledger.getTranscationDate()).y(ledger.getTranscation()).build());
                     ledgerModel.setPaymentMode(AMOUNT_RECEIVED.toString());
                     arSummary.setTotalReceived(ledger.getTranscation() + arSummary.getTotalReceived());
                     break;
                 case AMOUNT_RECEIVABLE:
-                    amount_receivable_list.add(0,DataSetBean.builder().x(ledger.getTranscationDate()).y(ledger.getTranscation()).build());
+                    amount_receivable_list.add(0, DataSetBean.builder().x(ledger.getTranscationDate()).y(ledger.getTranscation()).build());
                     ledgerModel.setPaymentMode(AMOUNT_RECEIVABLE.toString());
                     arSummary.setTotalReceivable(ledger.getTranscation() + arSummary.getTotalReceivable());
                     break;
             }
             ledgerModel.setAmount(ledger.getTranscation());
-            ledgerModel.setBalanceReceivable(invoiceID == null ?  ledger.getUserBalance():ledger.getInvoiceBalance());
+            ledgerModel.setBalanceReceivable(invoiceID == null ? ledger.getUserBalance() : ledger.getInvoiceBalance());
             ledgerModelList.add(ledgerModel);
         });
         if (ledgerList.size() > 0) {
@@ -71,9 +68,40 @@ public class ARService {
             arSummary.setTotalReceivable(arSummary.getTotalReceivable() + openingBalance);
         }
         arSummary.setTotalDue(arSummary.getTotalReceivable() - arSummary.getTotalReceived());
-        if (amount_receivable_list.size()==0) {
-            amount_receivable_list.add(DataSetBean.builder().x(startDate).y(arSummary.getOpeningBalance()).build());
+
+
+        // cumulative function for amount_received_list
+        for (int i = 0; i < amount_received_list.size() - 1; i++) {
+            amount_received_list.get(i + 1).setY(amount_received_list.get(i + 1).getY() + amount_received_list.get(i).getY());
         }
+
+        // cumulative function for amount_receivable_list
+        for (int i = 0; i < amount_receivable_list.size() - 1; i++) {
+            amount_receivable_list.get(i + 1).setY(amount_receivable_list.get(i + 1).getY() + amount_receivable_list.get(i).getY());
+        }
+
+        // set first and last data value for amount_receivable_list
+        if (amount_receivable_list.size() == 0) {
+            amount_receivable_list.add(DataSetBean.builder().x(startDate).y(arSummary.getOpeningBalance()).build());
+            amount_receivable_list.add(DataSetBean.builder().x(min(new Date(new java.util.Date().getTime()), endDate)).y(arSummary.getOpeningBalance()).build());
+        }
+
+        // set last data value for amount_received_list
+        if (amount_received_list.size() == 0) {
+            amount_received_list.add(DataSetBean.builder().x(startDate).y(0D).build());
+            amount_received_list.add(DataSetBean.builder().x(min(new Date(new java.util.Date().getTime()), endDate)).y(0D).build());
+        }
+
+        // add extra Points to match the graph to end data
+        DataSetBean last_receivable = amount_receivable_list.get(amount_receivable_list.size() - 1);
+        DataSetBean last_received = amount_received_list.get(amount_received_list.size() - 1);
+        addExtraEntry(last_receivable, last_received, amount_receivable_list, amount_received_list);
+
+        // uncomment below if you want dataset at same period for both amount_receivable_list and amount_received_list
+        //dataPointsAdapter(amount_receivable_list, amount_received_list);
+        System.out.println(amount_receivable_list);
+        System.out.println(amount_received_list);
+
         return AR360Model.builder()
                 .ledgerModelList(ledgerModelList)
                 .amount_receivable_list(amount_receivable_list)
@@ -82,5 +110,64 @@ public class ARService {
                 .build();
     }
 
+    private void dataPointsAdapter(List<DataSetBean> amount_receivable_list, List<DataSetBean> amount_received_list) {
+        int currentReceivablePosition = 0;
+        int currentReceivedPosition = 0;
+        java.util.Date checkingReceivableDate = amount_receivable_list.get(currentReceivablePosition).getX();
+        java.util.Date checkingReceivedDate = amount_received_list.get(currentReceivedPosition).getX();
 
+        boolean endNotReached = true;
+        do {
+            if (checkingReceivableDate.equals(checkingReceivedDate)) {
+                currentReceivablePosition++;
+                currentReceivedPosition++;
+                if (currentReceivablePosition >= amount_receivable_list.size() && currentReceivedPosition >= amount_received_list.size()) {
+                    endNotReached = false;
+                    continue;
+                }
+                checkingReceivableDate = amount_receivable_list.get(currentReceivablePosition).getX();
+                checkingReceivedDate = amount_received_list.get(currentReceivedPosition).getX();
+            } else if (checkingReceivableDate.before((checkingReceivedDate)) && currentReceivedPosition == 0) {
+                amount_received_list.add(currentReceivedPosition, new DataSetBean(checkingReceivableDate, 0D));
+                currentReceivablePosition++;
+                currentReceivedPosition++;
+                checkingReceivableDate = amount_receivable_list.get(currentReceivablePosition).getX();
+                checkingReceivedDate = amount_received_list.get(currentReceivedPosition).getX();
+            } else if (checkingReceivableDate.before((checkingReceivedDate))) {
+                amount_received_list.add(currentReceivedPosition, new DataSetBean(checkingReceivableDate, amount_received_list.get(currentReceivedPosition - 1).getY()));
+                currentReceivablePosition++;
+                currentReceivedPosition++;
+                checkingReceivableDate = amount_receivable_list.get(currentReceivablePosition).getX();
+                checkingReceivedDate = amount_received_list.get(currentReceivedPosition).getX();
+            } else if (checkingReceivedDate.before((checkingReceivableDate)) && currentReceivedPosition == 0) {
+                amount_receivable_list.add(currentReceivablePosition, new DataSetBean(checkingReceivedDate, 0D));
+                currentReceivablePosition++;
+                currentReceivedPosition++;
+                checkingReceivableDate = amount_receivable_list.get(currentReceivablePosition).getX();
+                checkingReceivedDate = amount_received_list.get(currentReceivedPosition).getX();
+            } else if (checkingReceivedDate.before((checkingReceivableDate))) {
+                amount_receivable_list.add(currentReceivablePosition, new DataSetBean(checkingReceivedDate, amount_receivable_list.get(currentReceivablePosition - 1).getY()));
+                currentReceivablePosition++;
+                currentReceivedPosition++;
+                checkingReceivableDate = amount_receivable_list.get(currentReceivablePosition).getX();
+                checkingReceivedDate = amount_received_list.get(currentReceivedPosition).getX();
+            }
+        } while (endNotReached);
+    }
+
+    private void addExtraEntry(DataSetBean last_receivable, DataSetBean
+            last_received, List<DataSetBean> amount_receivable_list, List<DataSetBean> amount_received_list) {
+        if (last_receivable.getX().before(last_received.getX())) {
+            amount_receivable_list.add(DataSetBean.builder().x(last_received.getX()).y(last_receivable.getY()).build());
+        } else {
+            amount_received_list.add(DataSetBean.builder().x(last_receivable.getX()).y(last_received.getY()).build());
+        }
+    }
+
+    private java.util.Date min(Date date, Date endDate) {
+        if (endDate.before(date)) {
+            return endDate;
+        }
+        return date;
+    }
 }
